a3ea3d6493fe012d4f22d4001652f6a6d297bf98
Oromion <caznaranl@uni.pe>
2018-02-04 23:16:26 -0500

    Commit

--- Primera clase/ejemplo2.R
+++ Primera clase/ejemplo2.R
@@ -1,3 +1,20 @@
+# ejemplo2.R
+#
+# Copyright Â© 2018 Oromion <caznaranl@uni.pe>
+#
+# This program is free software: you can redistribute it and/or modify
+# it under the terms of the GNU General Public License as published by
+# the Free Software Foundation, either version 3 of the License, or
+# (at your option) any later version.
+#
+# This program is distributed in the hope that it will be useful,
+# but WITHOUT ANY WARRANTY; without even the implied warranty of
+# MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+# GNU General Public License for more details.
+#
+# You should have received a copy of the GNU General Public License
+# along with this program.  If not, see <http://www.gnu.org/licenses/>.
+
 x <- 1:100
 x
 # library(?MASS)
@@ -10,7 +27,7 @@ bd[,2]
 c(4,4,3,2)
 x <- factor(1:3,labels = c("A","B","C"))
 x
-#c es la función concatenar.
+#c es la funciï¿½n concatenar.
 # No se debe cambiar los nombres de factor, labels y c.
 v <- c(1,1,1,2,2,2,3,3)
 x <- factor(v,labels = c("A","B","C"))
@@ -23,7 +40,7 @@ M1
 M2 <- matrix(1:36,6,6,byrow=TRUE)
 M2
 
-# Todo se convierte a texto, puesto que A es una letra y lo demás es número, predomina texto.
+# Todo se convierte a texto, puesto que A es una letra y lo demï¿½s es nï¿½mero, predomina texto.
 
 v <- c(1:35,"A")
 v
@@ -42,7 +59,7 @@ x<-data.frame(v1,v2,v3); x
 x$v1
 x[,2]
 # El data frame se carga por columnas.
-# En la línea 40 se ejecutan dos acciones.
+# En la lï¿½nea 40 se ejecutan dos acciones.
 
 # Creando una lista
 z<-c(3,2,8,9)
@@ -69,7 +86,7 @@ ts(1:47, frequency = 12,
 ts(1:47, frequency = 12,
    start = c(1959,3)) # empieza en marzo
 
-# Creando el objeto expresión
+# Creando el objeto expresiï¿½n
 
 exp1<- expression(x/(y+exp(z)))
 exp1
@@ -78,12 +95,12 @@ y<-3
 z<-5
 eval(exp1)
 
-# Conversión en R (qué son los archivos ocultos en R)
+# Conversiï¿½n en R (quï¿½ son los archivos ocultos en R)
 
 x<-c(TRUE,TRUE,FALSE);x #no lleva comillado
 as.numeric(x)
 
-x<-c("1","3","4") #cuando se use doble comillado será texto.
+x<-c("1","3","4") #cuando se use doble comillado serï¿½ texto.
 as.numeric(x)
 
 x<-c("A","B","C")
