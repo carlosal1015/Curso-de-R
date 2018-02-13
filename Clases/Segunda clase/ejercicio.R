adc6306694602cbbaaf46f8eb6948214d98a94f5
Oromion <caznaranl@uni.pe>
2018-02-13 12:50:10 -0500

    Initial commit

--- Clases/Segunda clase/ejercicio.R
+++ Clases/Segunda clase/ejercicio.R
@@ -1,1 +1,9 @@
-# Cuando usamos incorrectamente 
+# Cuando usamos incorrectamente && en lugar de
+# & para est tipo de casos, R
+# simplemente evalúa los primeros resultados
+# de los vectores l+ogicos e ignora el resto. En general
+# && es pereferido por el control de flujo, es decir,
+# el uso de los condicionales if o while
+remove('x')
+x <- rbinom(100,5,0.80)
+x[(x>1)&(x<3)]
